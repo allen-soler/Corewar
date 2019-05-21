d4b4ec5478f106add025473fd1e38673ee5e21a4
Néstor Alonso Viña <alonsovinanestor@gmail.com>
2019-05-21 16:08:33 +0200

    modified op.h

--- includes/op.h
+++ includes/op.h
@@ -1,76 +1,64 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   op.h                                               :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: zaz <marvin@42.fr>                         +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2013/10/04 11:33:27 by zaz               #+#    #+#             */
-/*   Updated: 2019/05/21 14:26:13 by nalonso          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 /*
  * ** Toutes les tailles sont en octets.
  * ** On part du principe qu'un int fait 32 bits. Est-ce vrai chez vous ?
  * */
 
-#define IND_SIZE2
-#define REG_SIZE4
-#define DIR_SIZEREG_SIZE
+#define IND_SIZE				2
+#define REG_SIZE				4
+#define DIR_SIZE				REG_SIZE
 
 
-# define REG_CODE1
-# define DIR_CODE2
-# define IND_CODE3
+# define REG_CODE				1
+# define DIR_CODE				2
+# define IND_CODE				3
 
 
-#define MAX_ARGS_NUMBER4
-#define MAX_PLAYERS4
-#define MEM_SIZE(4*1024)
-#define IDX_MOD(MEM_SIZE / 8)
-#define CHAMP_MAX_SIZE(MEM_SIZE / 6)
+#define MAX_ARGS_NUMBER			4
+#define MAX_PLAYERS				4
+#define MEM_SIZE				(4*1024)
+#define IDX_MOD					(MEM_SIZE / 8)
+#define CHAMP_MAX_SIZE			(MEM_SIZE / 6)
 
-#define COMMENT_CHAR'#'
-#define LABEL_CHAR':'
-#define DIRECT_CHAR'%'
-#define SEPARATOR_CHAR','
+#define COMMENT_CHAR			'#'
+#define LABEL_CHAR				':'
+#define DIRECT_CHAR				'%'
+#define SEPARATOR_CHAR			','
 
-#define LABEL_CHARS"abcdefghijklmnopqrstuvwxyz_0123456789"
+#define LABEL_CHARS				"abcdefghijklmnopqrstuvwxyz_0123456789"
 
-#define NAME_CMD_STRING".name"
-#define COMMENT_CMD_STRING".comment"
+#define NAME_CMD_STRING			".name"
+#define COMMENT_CMD_STRING		".comment"
 
-#define REG_NUMBER16
+#define REG_NUMBER				16
 
-#define CYCLE_TO_DIE1536
-#define CYCLE_DELTA50
-#define NBR_LIVE21
-#define MAX_CHECKS10
-
-/*
- * **
- * */
-
-typedef chart_arg_type;
-
-#define T_REG1
-#define T_DIR2
-#define T_IND4
-#define T_LAB8
+#define CYCLE_TO_DIE			1536
+#define CYCLE_DELTA				50
+#define NBR_LIVE				21
+#define MAX_CHECKS				10
 
 /*
- * **
- * */
+**
+*/
 
-# define PROG_NAME_LENGTH(128)
-# define COMMENT_LENGTH(2048)
-# define COREWAR_EXEC_MAGIC0xea83f3
+typedef char	t_arg_type;
 
-typedef structheader_s
+#define T_REG					1
+#define T_DIR					2
+#define T_IND					4
+#define T_LAB					8
+
+/*
+**
+*/
+
+# define PROG_NAME_LENGTH		(128)
+# define COMMENT_LENGTH			(2048)
+# define COREWAR_EXEC_MAGIC		0xea83f3
+
+typedef struct		header_s
 {
-	  unsigned intmagic;
-	    charprog_name[PROG_NAME_LENGTH + 1];
-		  unsigned intprog_size;
-		    charprog_namecomment[COMMENT_LENGTH + 1];
-}PROG_NAME_LENGTHheader_t;
+  unsigned int		magic;
+  char				prog_name[PROG_NAME_LENGTH + 1];
+  unsigned int		prog_size;
+  char				comment[COMMENT_LENGTH + 1];
+}					header_t;
