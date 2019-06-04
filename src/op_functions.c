2b3a145c645161f3b39fe222b417b6b24f1720c8
Néstor Alonso Viña <alonsonestor20@gmail.com>
2019-06-04 14:43:04 +0200

    documented the functions and moved them to the top

--- src/op_functions.c
+++ src/op_functions.c
@@ -1,5 +1,97 @@
 #include "../includes/vm.h"
 
+void	reset_args(t_process *cursor)
+{
+	u_int8_t	i;
+
+	i = 0;
+	while (i < MAX_ARGS_NUMBER)
+	{
+		cursor->args[i].type = 0;
+		cursor->args[i].type = 0;
+		i += 1;
+	}
+}
+
+/*
+**	Function: mix_bytes
+**	Takes an index and the number of bytes you want to mix
+**	and it'll mix those bytes into one number.
+*/
+
+int		mix_bytes(t_env *e, int index, int len)
+{
+	int	res;
+	int	i;
+
+	i = 0;
+	res = 0;
+	while (i < len)
+	{
+		res = (res << 8) | e->arena[index++].data;
+		i += 1;
+	}
+	return (res);
+}
+
+/*
+**	Function: read_args
+**	This function will read the enconding byte if it exists and read
+**	the associated arguments afterwards, it supposes the cursor->pc
+**	is set to the instructions index.
+**	PD: THIS FUNCTION SUPPOSES A PREVIOUS CHECK THAT THE INSTRUCTION HAS A
+**	ENCODING BYTE, if the op doesn't have a encoding byte, just use mixe_bytes,
+**	with the correct len associeated to the expected argument.
+*/
+
+void	read_args(t_env *e, t_process *cursor, t_op op)
+{
+	u_int8_t	i;
+	u_int8_t	type;
+	int			arg_len;
+	int			offset;
+
+	offset = 2;
+	reset_args(cursor);
+	i = 0;
+	while (i < op.param_nb)
+	{
+		type = e->arena[cursor->pc + 1].data;
+		if (type >> (i * 2) != 0b0)
+		{
+			/*
+			** In this bitwise operation we are taking the byte that
+			** represents the arguments and taking the last two bytes (& 3 should do the work),
+			** after previously shifting to right in reversed order (type >> decresing_size).
+			*/
+
+			type = (type >> ((op.param_nb - i) * 2) & 3);
+			cursor->args[i].type = type;
+		
+			/*
+			** An argument can have a different size depending on both it's
+			** type and the op_code.
+			** For example a direct type (T_DIR) can have a size of 4 or 2 bytes,
+			** while an indirect type (T_IND) will always have a size of 2 bytes
+			** and a register (T_REG) will always have a size of 1 byte.
+			*/
+
+			arg_len = 1;
+			if ((op.direct_size == 1 && type == T_DIR) || type == T_IND)
+			{
+				arg_len = 2;
+			}
+			else if (type == T_DIR && op.direct_size == 0)
+			{
+				arg_len = 4;
+			}
+			cursor->args[i].value = mix_bytes(e, cursor->pc + offset, arg_len);
+			offset += arg_len;
+		}
+		i += 1;
+	}
+}
+
 void	ft_live(t_env *e, t_process *cursor, t_op op)
 {
 	int i;
@@ -64,81 +156,6 @@ void	ft_ldi(t_env *e, t_process *cursor, t_op op)
 **	ARGUMENTS :=
 */
 
-void	reset_args(t_process *cursor)
-{
-	u_int8_t	i;
-
-	i = 0;
-	while (i < MAX_ARGS_NUMBER)
-	{
-		cursor->args[i].type = 0;
-		cursor->args[i].type = 0;
-		i += 1;
-	}
-}
-
-int		mix_bytes(t_env *e, int index, int len)
-{
-	int	res;
-	int	i;
-
-	i = 0;
-	res = 0;
-	while (i < len)
-	{
-		res = (res << 8) | e->arena[index++].data;
-		i += 1;
-	}
-	return (res);
-}
-
-void	read_args(t_env *e, t_process *cursor, t_op op)
-{
-	u_int8_t	i;
-	u_int8_t	type;
-	int			arg_len;
-	int			offset;
-
-	offset = 2;
-	reset_args(cursor);
-	i = 0;
-	while (i < op.param_nb)
-	{
-		type = e->arena[cursor->pc + 1].data;
-		if (type >> (i * 2) != 0b0)
-		{
-			/*
-			** In this bitwise operation we are taking the byte that
-			** represents the arguments and taking the last two bytes (& 3 should do the work),
-			** after previously shifting to right in reversed order (type >> decresing_size).
-			*/
-
-			type = (type >> ((op.param_nb - i) * 2) & 3);
-			cursor->args[i].type = type;
-		
-			/*
-			** An argument can have a different size depending on both it's
-			** type and the op_code.
-			** For example a direct type (T_DIR) can have a size of 4 or 2 bytes,
-			** while an indirect type (T_IND) will always have a size of 2 bytes
-			** and a register (T_REG) will always have a size of 1 byte.
-			*/
-
-			arg_len = 1;
-			if ((op.direct_size == 1 && type == T_DIR) || type == T_IND)
-			{
-				arg_len = 2;
-			}
-			else if (type == T_DIR && op.direct_size == 0)
-			{
-				arg_len = 4;
-			}
-			cursor->args[i].value = mix_bytes(e, cursor->pc + offset, arg_len);
-			offset += arg_len;
-		}
-		i += 1;
-	}
-}
 
 void	ft_sti(t_env *e, t_process *cursor, t_op op)
 {
