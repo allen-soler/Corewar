f6385a81a59378e589f096671611b878308c2c0e
basselg53@gmail.com <bghandou@e3r8p15.42.fr>
2019-06-30 13:56:59 +0200

    debug separated arguments by space

--- srcs/check_comma.c
+++ srcs/check_comma.c
@@ -6,7 +6,7 @@
 /*   By: bghandou <bghandou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/25 19:40:00 by bghandou          #+#    #+#             */
-/*   Updated: 2019/06/30 12:56:41 by bghandou         ###   ########.fr       */
+/*   Updated: 2019/06/30 13:56:13 by bghandou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,19 +34,22 @@ static int	check_end_comma(char *line)
 static int	space_comma(char *line, size_t i, t_par **list)
 {
 	size_t	count;
+	size_t	j;
 
 	count = 0;
-	while ((line[i] == ' ' || line[i] == ',') && count <= 1)
+	j = 0;
+	while ((line[i + j] == ' ' || line[i + j] == '\t' || line[i + j] == ',')
+			&& count <= 1)
 	{
-		if (line[i] == ',')
+		if (line[i + j] == ',')
 			count++;
-		i++;
+		j++;
 	}
-	if (count == 1 || (count == 0 && line[i] == '\0'))
-		return (i);
+	if (count == 1 || (count == 0 && line[i + j] == '\0'))
+		return (j);
 	else
 		error_function(NULL, list);
-	return (i);
+	return (j);
 }
 
 int		check_comma(char *line, t_par **list)
@@ -56,17 +59,17 @@ int		check_comma(char *line, t_par **list)
 	int		err;
 	size_t	len;
 
-	i = -1;
+	i = 0;
 	count = 0;
 	err = 0;
 	len = ft_strlen(line);
-	while (line && line[++i] == ' ')
-		;
+	while (line && (line[i] == ' ' || line[i] == '\t'))
+		i++;
 	if (line[i] == ',')
 		return (1);
 	while (i <= len && line[i] != '\0' && err == 0)
 	{
-		if (line[i] == ' ' || line[i] == ',')
+		if (line[i] == ' ' || line[i] == ',' || line[i] == '\t')
 			i += space_comma(line, i, list);
 		i++;
 	}
