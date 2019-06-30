f6ad399de0c7115f76c9a5d1f9e32bfda1e04d93
basselg53@gmail.com <bghandou@e3r8p15.42.fr>
2019-06-30 19:12:50 +0200

    slight clean of classes

--- srcs/syntax/syntax_checker.c
+++ srcs/syntax/syntax_checker.c
@@ -6,7 +6,7 @@
 /*   By: bghandou <bghandou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/27 17:43:21 by bghandou          #+#    #+#             */
-/*   Updated: 2019/06/30 19:11:16 by bghandou         ###   ########.fr       */
+/*   Updated: 2019/06/30 19:12:24 by bghandou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,25 +28,6 @@ t_par	*fetch_function(t_par *tmp, int class, t_par *head)
 	class_arr[7] = class_8;
 	class_arr[8] = class_9;
 	tmp = class_arr[class - 1](tmp, head);
-/*	if (class == 1)
-		tmp = class_1(tmp, head);
-	else if (class == 2)
-		tmp = class_2(tmp, head);
-	else if (class == 3)
-		tmp = class_3(tmp, head);
-	else if (class == 4)
-		tmp = class_4(tmp, head);
-	else if (class == 5)
-		tmp = class_5(tmp, head);
-	else if (class == 6)
-		tmp = class_6(tmp, head);
-	else if (class == 7)
-		tmp = class_7(tmp, head);
-	else if (class == 8)
-		tmp = class_8(tmp, head);
-	else if (class == 9)
-		tmp = class_9(tmp, head);
-		*/
 	return (tmp);	
 }
 
