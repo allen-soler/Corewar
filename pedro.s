48dc224362e4446fc952f04e87b0f31ec5e143bb
allespag <alexandre.lespagnol401@gmail.com>
2019-07-10 19:06:08 +0200

    pedro is getting stronger

--- pedro.s
+++ pedro.s
@@ -1,4 +1,60 @@
 .name "pedro"
-.comment "monfort"
+.comment "monfort el gabacho"
 
-live %2
+# DU COUP, LE 04 PROCHE DE L'INFINIT LOOP N'EST PAS UN AND MAIS UN ARGS DU COUP CHIANT ^^^^^^^^^^
+
+and %0, %1, r16
+ld %57672449, r3	# CORROMPEMOS	NUMBER
+ld %17367049, r4	# PROTEGEMOS	NUMBER
+fork %:inicializamos
+
+atacamos:
+	st r3, 350
+	st r3, 350
+	st r3, 350
+	st r3, 350
+	st r3, 350	
+	st r3, 350
+	st r3, 350
+	st r3, 350
+	st r3, 350
+	st r3, 350
+	st r3, 350
+	st r3, 350
+	st r3, 350
+	st r3, 350
+	st r3, 350
+	st r3, 350
+	st r3, 350
+	st r3, 350
+	st r3, 350
+	st r3, 350
+	st r3, 350
+	st r3, 350
+	st r3, 350
+	st r3, 350
+	st r3, 350
+	st r3, 350
+	st r3, 350
+	st r3, 350
+	st r3, 350
+	st r3, 350
+	st r3, 350
+	st r3, 350
+	st r3, 350
+	st r3, 350
+	st r3, 350
+	st r3, 350
+	st r3, 350
+	st r3, 350
+	st r3, 350
+	st r3, 350
+
+inicializamos:
+	and %0, %0, r16
+	sti r1, %:vivimos, %1
+	sti r4, %-8, %0
+
+vivimos:
+	live %-14
+	zjmp %:vivimos
