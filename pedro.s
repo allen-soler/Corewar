74f6d82925364e34342016f0d8706d0a1b79b044
allespag <alexandre.lespagnol401@gmail.com>
2019-07-11 18:15:32 +0200

    pedro is the worst champion ever made

--- pedro.s
+++ pedro.s
@@ -1,60 +1,123 @@
 .name "pedro"
 .comment "monfort el gabacho"
 
-# DU COUP, LE 04 PROCHE DE L'INFINIT LOOP N'EST PAS UN AND MAIS UN ARGS DU COUP CHIANT ^^^^^^^^^^
-
-and %0, %1, r16
-ld %57672449, r3	# CORROMPEMOS	NUMBER
-ld %17367049, r4	# PROTEGEMOS	NUMBER
+ld %0, r10
+ld %57672449, r3	# CORROMPEMOS:	st r3, 350
+ld %589833, r4		# PROTEGEMOS:	zjmp %9
+and %0, %0, r10
 fork %:inicializamos
+and %1, %1, r16
+zjmp %:atacamos
+
+dormimos:
+	st r10, -500
+	st r10, -500
+	st r10, -500
+	st r10, -500
+	st r10, -500
+	st r10, -500
+	st r10, -500
+	st r10, -500
+	st r10, -500
+	st r10, -500
+	st r10, -500
+	st r10, -500
+	st r10, -500
+	st r10, -500
+	st r10, -500
+	st r10, -500
+	st r10, -500
+	st r10, -500
+	st r10, -500
+	st r10, -500
+	st r10, -500
+	st r10, -500
+	st r10, -500
+	st r10, -500
+	st r10, -500
+	st r10, -500
+	st r10, -500
+	st r10, -500
+	st r10, -500
+	st r10, -500
+	st r10, -500
+	st r10, -500
 
 atacamos:
-	st r3, 350
-	st r3, 350
-	st r3, 350
-	st r3, 350
-	st r3, 350	
-	st r3, 350
-	st r3, 350
-	st r3, 350
-	st r3, 350
-	st r3, 350
-	st r3, 350
-	st r3, 350
-	st r3, 350
-	st r3, 350
-	st r3, 350
-	st r3, 350
-	st r3, 350
-	st r3, 350
-	st r3, 350
-	st r3, 350
-	st r3, 350
-	st r3, 350
-	st r3, 350
-	st r3, 350
-	st r3, 350
-	st r3, 350
-	st r3, 350
-	st r3, 350
-	st r3, 350
-	st r3, 350
-	st r3, 350
-	st r3, 350
-	st r3, 350
-	st r3, 350
-	st r3, 350
-	st r3, 350
-	st r3, 350
-	st r3, 350
-	st r3, 350
-	st r3, 350
+	st r3, 500
+	st r3, 500
+	st r3, 500
+	st r3, 500
+	st r3, 500
+	st r3, 500
+	st r3, 500
+	st r3, 500
+	st r3, 500
+	st r3, 500
+	st r3, 500
+	st r3, 500
+	st r3, 500
+	st r3, 500
+	st r3, 500
+	st r3, 500
+	st r3, 500
+	st r3, 500
+	st r3, 500
+	st r3, 500
+	st r3, 500
+	st r3, 500
+	st r3, 500
+	st r3, 500
+	st r3, 500
+	st r3, 500
+	st r3, 500
+	st r3, 500
+	st r3, 500
+	st r3, 500
+	st r3, 500
+	st r3, 500
+	st r3, 500
+	st r3, 500
+	st r3, 500
+	st r3, 500
+	st r3, 500
+	st r3, 500
+	st r3, 500
+	st r3, 500
+	st r3, 500
+	st r3, 500
+	st r3, 500
+	st r3, 500
+	st r3, 500
+	st r3, 500
+	st r3, 500
+	st r3, 500
+	st r3, 500
+	st r3, 500
+	st r3, 500
+	st r3, 500
+	st r3, 500
+	st r3, 500
+	st r3, 500
+	st r3, 500
+	st r3, 500
+	st r3, 500
+	st r3, 500
+	sti r1, %:bebemos, %1
+
+bebemos:
+	live %-14
+	and %0, %0, r10
+	zjmp %:saltamos
 
 inicializamos:
-	and %0, %0, r16
 	sti r1, %:vivimos, %1
-	sti r4, %-8, %0
+	sti r4, %-7, %0
+	and %0, %0, r10
 
 vivimos:
 	live %-14
 	zjmp %:vivimos
+
+saltamos:
+	zjmp %146
