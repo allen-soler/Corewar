21ff92e2d54bcc532c06880dce64e4d7a761cc23
Nestor ALONSO <nalonso@e1r1p12.42.fr>
2019-07-13 11:10:30 +0200

    norme

--- srcs_vm/set_players.c
+++ srcs_vm/set_players.c
@@ -3,14 +3,36 @@
 /*                                                        :::      ::::::::   */
 /*   set_players.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: allespag <allespag@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nalonso <nalonso@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/09 16:23:51 by allespag          #+#    #+#             */
-/*   Updated: 2019/07/12 16:09:50 by allespag         ###   ########.fr       */
+/*   Updated: 2019/07/13 11:08:47 by nalonso          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/vm.h"
+
+void		set_players2(int *turn_ptr, int *used_ptr, t_env *env, int *i)
+{
+	int turn;
+	int used;
+
+	turn = *turn_ptr;
+	used = *used_ptr;
+	turn = choose_turn(used, env);
+	if (turn == -1)
+		set_error_exit(env, ERROR_SME_NUMB);
+	else
+	{
+		env->players[turn].parse_index = *i;
+		env->players[turn].alive = 0;
+		env->players_nb++;
+		used |= (1 << turn);
+	}
+	(*i)++;
+	*turn_ptr = turn;
+	*used_ptr = used;
+}
 
 void		set_players(char **av, int ac, int i, t_env *env)
 {
@@ -35,19 +57,7 @@ void		set_players(char **av, int ac, int i, t_env *env)
 			}
 		}
 		else if (ft_endswith(av[i], ".cor") == 1)
-		{
-			turn = choose_turn(used, env);
-			if (turn == -1)
-				set_error_exit(env, ERROR_SME_NUMB);
-			else
-			{
-				env->players[turn].parse_index = i;
-				env->players[turn].alive = 0;
-				env->players_nb++;
-				used |= (1 << turn);
-			}
-			i++;
-		}
+			set_players2(&turn, &used, env, &i);
 		else
 			set_error_exit(env, ERROR_UNK_ARGV);
 	}
