b48b83e359f00aea2b102b80a4ce779743ee2cf1
Jorge Allen <jallen@student.42.fr>
2019-07-13 11:48:22 +0200

    strcmp!

--- libft/src/libstr/ft_strcmp.c
+++ libft/src/libstr/ft_strcmp.c
@@ -6,16 +6,22 @@
 /*   By: awoimbee <awoimbee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/26 17:58:22 by awoimbee          #+#    #+#             */
-/*   Updated: 2019/04/28 04:51:31 by awoimbee         ###   ########.fr       */
+/*   Updated: 2019/07/13 11:47:18 by jallen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int		ft_strcmp(const char *s1, const char *s2)
 {
-	while (*s1 == *s2 && (*s1 != '\0' || *s2 != '\0'))
+	int i;
+
+	i = 0;
+	if (!s1 || !s2)
+		return (0);
+	while ((s1[i] || s2[i]) != '\0')
 	{
-		++s1;
-		++s2;
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		i++;
 	}
-	return ((unsigned char)*s1 - (unsigned char)*s2);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
